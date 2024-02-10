3ad45b917757f0dc7c1b2be7b3653ff3a170e00a
torwar02 <ziongassner02@g.ucla.edu>
2024-02-09 17:04:26 -0800

    Deleted main template, added comments, removed unnecessary imports

--- flask-project/app.py
+++ flask-project/app.py
@@ -1,76 +1,65 @@
-'''
-At the command line, run 
-
-conda activate PIC16B-24W
-export FLASK_ENV=development
-flask run
-
-# Sources
-
-This set of lecture notes is based in part on previous materials developed by [Erin George](https://www.math.ucla.edu/~egeo/) (UCLA Mathematics) and the tutorial [here](https://stackabuse.com/deploying-a-flask-application-to-heroku/). 
-'''
 import sqlite3
 import pandas as pd
 
-from flask import g, Flask, render_template, request #Flask class is the most important, other things just help out
-from flask import redirect, url_for, abort
+from flask import Flask, g, render_template, request #Flask is most important, others are for functions
+from flask import redirect, url_for
 
 app = Flask(__name__) #Making a Flask object with `__name`--all HTML files go here.
 
-# www.google.com/
-@app.route("/") # decorators #Route method of the Flask instance. Decorators are functions that modify functions.
+@app.route("/") # This is a decorator
 def redirect_page():
-    return redirect(url_for("submit"))
+    return redirect(url_for("submit")) #Immediately take people to `submit`
 
 
-@app.route("/submit/", methods=['POST', 'GET'])
+@app.route("/submit/", methods=['POST', 'GET']) #Two methods (before/after post)
 def submit():
     if request.method == 'GET':
-        return render_template('submit.html')
+        return render_template('submit.html') #Render `submit.html` as-is at first
     else:
-        handle, message = insert_message(request)
+        handle, message = insert_message(request) #When submitted, get back message info
         return render_template('submit.html', sec = True, handle = handle, message = message)
-
+        #Return template again but with "message sent" displayed.
+        
 @app.route("/view/")
-def view():
-    postings = random_messages(5)
+def view(): #For viewing messages
+    postings = random_messages(5) #Get 5 random messages from database
     length = 5
     message_tuples = []
     for i in range(length):
-        message_tuples.append(tuple(postings.iloc[i,:]))
-    return render_template('view.html', message_tuples = message_tuples)
+        message_tuples.append(tuple(postings.iloc[i,:])) #Convert df rows into tuples
+    return render_template('view.html', message_tuples = message_tuples) #display `view`
     
     
-def get_message_db():
+def get_message_db(): #create/access database
     try:
-        return g.message_db
-    except:
-        g.message_db = sqlite3.connect("messages_db.sqlite")
+        return g.message_db #gets datbase from `g` object
+    except: #should only go through here once
+        g.message_db = sqlite3.connect("messages_db.sqlite") #makes database
         cursor = g.message_db.cursor()
         cmd = """
         CREATE TABLE IF NOT EXISTS messages(
         handle TEXT,
         message TEXT
         );
-        """
+        """ #creates table
         cursor.execute(cmd)
         cursor.close()
-        return g.message_db
+        return g.message_db #then returns datbaase
         
-def insert_message(request):
-    handle = request.form['handle']
+def insert_message(request): #places messages into database
+    handle = request.form['handle'] #extracts `handle`/`message` from form
     message = request.form['message']
     db = get_message_db()
-    info = pd.DataFrame([{'handle': handle, 'message' : message}])
-    info.to_sql("messages", db, if_exists = "append", index = False)
+    info = pd.DataFrame([{'handle': handle, 'message' : message}]) #places handle/message into df
+    info.to_sql("messages", db, if_exists = "append", index = False) #places into db
     db.commit()
     db.close()
-    return handle, message
+    return handle, message #sends back `handle` and `message` to `submit()`
     
-def random_messages(n):
+def random_messages(n): #set to 5 in `view()`
     db = get_message_db()
-    cmd = f""" SELECT * FROM messages ORDER BY RANDOM() LIMIT {n}; """
-    postings = pd.read_sql_query(cmd, db)
+    cmd = f""" SELECT * FROM messages ORDER BY RANDOM() LIMIT {n}; """ #grabs `n` random messages
+    postings = pd.read_sql_query(cmd, db) #sent to df
     db.close()
-    return postings
+    return postings #returned to `view()`
     
